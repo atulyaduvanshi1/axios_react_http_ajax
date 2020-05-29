c07f02203cd55b07426636b236626a329facfaef
atulyaduvanshi <atul.kumar5@allocatesoftware.com>
2020-05-29 17:45:46 +0530

    Remove the now ignored directory node_modules

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
+
+// axios.interceptors.request.use(
+//   (request) => {
+//     console.log(request);
+//     return request;
+//   },
+//   (error) => {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
+
+// axios.interceptors.request.use(
+//   (response) => {
+//     console.log(response);
+//   },
+//   (error) => {
+//     console.log(error);
+
+//     return Promise.reject(error);
+//   }
+// );
+// axios.defaults.headers.common['Authorization']='AUTH TOKEN'
+
+// axios.defaults.headers.post['Content-Type']='application/json'
+
+axios.defaults.baseURL='https://jsonplaceholder.typicode.com';
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
